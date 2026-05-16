59c6f65312c38fdc1389e7ba6d3b4f78c53d66c4
Sophie Stebler <steblerstudios@gmail.com>
2026-05-16 16:31:29 +0200

    feat: export workflow runtime

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,3 +1,6 @@
 export * from "./types";
 export * from "./validation/workflow-validator";
+export * from "./events/event-bus";
+export * from "./core/workflow-runtime";export * from "./types";
+export * from "./validation/workflow-validator";
 
